54df64a9e131bd82a0c31590546d21027912d499
sagar singh <sagarsinghkoranga@gmail.com>
2026-01-28 15:15:53 +0530

    register10

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL = import.meta.env.production.VITE_API_URL;
  export const api = axios.create({
   baseURL:  API_URL,
 });
